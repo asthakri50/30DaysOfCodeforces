71642bb2c7e62b67e234d7fa0391a156c8b3fdda
Astha Kumari <astha.kri50@gmail.com>
2021-01-27 23:14:56 +0530

    Day3

---
+++ Day3_572A.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class Day3_572A {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.nextLine();
+        int[] A = new int[a];
+        int[] B = new int[b];
+
+        int k = scan.nextInt();
+        int m = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 0; i < a; i++)
+            A[i] = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 0; i < b; i++)
+            B[i] = scan.nextInt();
+        scan.close();
+        if (A[k - 1] < B[b - m])
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+}
