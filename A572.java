051171b5ed20c10d3c2058fc7e9099bc943013a8
Astha Kumari <astha.kri50@gmail.com>
2021-02-23 15:58:41 +0530

    sortings

--- A572.java
+++ A572.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 import javax.lang.model.util.ElementScanner6;
 
-public class Day3_572A {
+public class A572 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
