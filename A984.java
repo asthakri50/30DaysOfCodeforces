f459b34a86e5c76257aa3e76bbe1a881aa955585
Astha Kumari <astha.kri50@gmail.com>
2021-02-23 15:55:31 +0530

    sortings

---
+++ A984.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class A984 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scan.nextInt();
+        scan.close();
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[(n - 1) / 2]);
+
+    }
+}
