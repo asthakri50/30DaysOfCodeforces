bdc3f2a324e31108abbf72d3329c603d1c7fad3b
Astha Kumari <astha.kri50@gmail.com>
2021-02-23 16:25:44 +0530

    greedy, sortings

--- A160.java
+++ A160.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Day7_160A {
+public class A160 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
